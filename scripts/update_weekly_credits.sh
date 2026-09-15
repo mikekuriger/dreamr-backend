8ce3db3e0b41617f4ea1548b338d1e774f6eeea2
Mike Kuriger <mikekuriger@gmail.com>
2026-09-15 16:14:52 -0700

    updated free weekly credits to 1 for new users, old users still get 2.

--- scripts/update_weekly_credits.sh
+++ scripts/update_weekly_credits.sh
@@ -3,7 +3,15 @@
 export PATH=/home/mk7193/dart-dreamr/flutter/bin:/opt/dreamr-venv/bin:/usr/bin:/home/mk7193/.local/bin:/home/mk7193/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 
 date=$(date "+%Y-%m-%d %H:%M:00")
-mysql -u root -ppay4mysql dreamr -e 'select * from user_credits;'
 #mysql -u root -ppay4mysql dreamr -e 'update user_credits set text_remaining_week=2,updated_at = NOW();'
-mysql -u root -ppay4mysql dreamr -e 'update user_credits set free_credits=2,updated_at = NOW();'
-mysql -u root -ppay4mysql dreamr -e 'select * from user_credits;'
+#mysql -u root -ppay4mysql dreamr -e 'update user_credits set free_credits=2,updated_at = NOW();'
+
+mysql -u root -ppay4mysql dreamr <<'SQL'
+UPDATE user_credits uc
+JOIN users u ON u.id = uc.user_id
+SET uc.free_credits = CASE
+    WHEN u.signup_date > '2026-05-01' THEN 1
+    ELSE 2
+END,
+uc.updated_at = NOW();
+SQL
